a91775c5c159cfb5ebc3312d00599e4b761a649e
liuyiye <133562379+liuyiye@users.noreply.github.com>
2023-12-21 07:38:26 -0800

    Add files via upload

--- z.py
+++ z.py
@@ -4,6 +4,7 @@ import slicer
 from cv2 import minAreaRect
 from skimage import measure
 from skimage.filters import threshold_otsu
+from scipy.spatial.distance import pdist, squareform
 from collections import Counter
 
 
@@ -25,7 +26,7 @@ def max2d(mask):
     return(max_z,round(max_length/10,1),round(width/10,1),round(mask[max_z,:,:].sum()/100,2))
 
 
-#三维mask轴位图像最大面积所在平面的最小外接2D矩形
+#三维mask轴位图像最大面积所在平面的最小外接2D矩形、真实长径及相应的坐标
 def max_area(mask):
     coords3d = np.array(np.where(mask)).T
     z_counter = Counter([p[0] for p in coords3d])
@@ -36,7 +37,10 @@ def max_area(mask):
     rect=minAreaRect(coords2d)
     max_length=max(rect[1][0],rect[1][1])
     width=min(rect[1][0],rect[1][1])
-    return(max_z,round(max_length/10,1),round(width/10,1),round(max_area/100,2))
+    true=true_max3d(slice_mask)
+    p1=[max_z,true[1][0],true[1][1]]
+    p2=[max_z,true[2][0],true[2][1]]
+    return(max_z,round(max_length/10,1),round(width/10,1),round(max_area/100,2),round(true[0]/10,2),p1,p2)
 
 
 #三维mask最小外接3D柱体
@@ -52,7 +56,72 @@ def max3d(mask):
     return(L,W,H) #返回最大三维长径、宽度、高度
 
 
-#白质高信号定量
+#计算三维mask的边缘
+def find_edge_3d(mask):
+    # 沿着6个方向分别平移，因为mask不会出现在最边缘，就不用考虑卷折到另一边
+    shifted1 = np.roll(mask, 1, axis=0)
+    shifted2 = np.roll(mask, -1, axis=0)
+    shifted3 = np.roll(mask, 1, axis=1)
+    shifted4 = np.roll(mask, -1, axis=1)
+    shifted5 = np.roll(mask, 1, axis=2)
+    shifted6 = np.roll(mask, -1, axis=2)
+    
+    # 计算差异，找到边缘。-1 or 1 = -1,所以使用clip将结果限制在0和1。也可用mask.astype(bool)，-1 or 1 就等于1了。前者代码更简单。
+    edge_mask = (mask - shifted1) | (mask - shifted2) | (mask - shifted3) | (mask - shifted4)| (mask - shifted5) | (mask - shifted6)
+    edge_mask =edge_mask.clip(0, 1)
+    return edge_mask
+
+
+#计算二维mask的边缘
+def find_edge_2d(mask):
+    # 沿着4个方向分别平移，因为mask不会出现在最边缘，就不用考虑卷折到另一边
+    shifted1 = np.roll(mask, 1, axis=0)
+    shifted2 = np.roll(mask, -1, axis=0)
+    shifted3 = np.roll(mask, 1, axis=1)
+    shifted4 = np.roll(mask, -1, axis=1)
+    
+    # 计算差异，找到边缘。-1 or 1 = -1,所以使用clip将结果限制在0和1。也可用mask.astype(bool)，-1 or 1 就等于1了。前者代码更简单。
+    edge_mask = (mask - shifted1) | (mask - shifted2) | (mask - shifted3) | (mask - shifted4)
+    edge_mask =edge_mask.clip(0, 1)
+    return edge_mask
+
+
+#计算三维最大径及相应的坐标(传入edge_mask以减少计算量)
+def true_max3d(mask):
+    coords = np.array(np.where(mask)).T
+    distances = pdist(coords)
+    max_distance=distances.max()
+    
+    distance_matrix = squareform(distances)
+    max_distance_index = np.argmax(distance_matrix)
+    i, j = np.unravel_index(max_distance_index, distance_matrix.shape)
+    point1 = coords[i]
+    point2 = coords[j]
+    return(max_distance,point1,point2)
+
+
+#计算轴位最大径、所在层面及相应的坐标
+def true_max2d(mask):
+    z_slices = np.unique(np.where(mask)[0]) 
+    
+    max_2d_in_slice = 0
+    max_2d_z_value = 0
+    point1 = None
+    point2 = None
+    
+    for z in z_slices:
+        slice_mask = find_edge_2d(mask[z,:, :])
+        temp=true_max3d(slice_mask)
+        if max_2d_in_slice < temp[0]:
+            max_2d_in_slice = temp[0]
+            max_2d_z_value = z
+            point1=[z,temp[1][0],temp[1][1]]
+            point2=[z,temp[2][0],temp[2][1]]
+    
+    return(max_2d_z_value,max_2d_in_slice,point1,point2)
+
+
+#main()白质高信号定量
 def w():
     brain_lobe= {
     1:'右侧额叶',
@@ -102,9 +171,13 @@ def w():
     max_flair=flair.copy()
     max_flair[:]=0
     max_flair[labels==flair_sorted[0]]=1
+    
     max_flair_2d=max2d(max_flair)
     max_flair_area=max_area(max_flair)
     max_flair_3d=max3d(max_flair)
+    
+    true_max_flair_3d=true_max3d(find_edge_3d(max_flair))
+    true_max_flair_2d=true_max2d(max_flair)
     
     mask1=mask.copy()
     mask1[brain==0]=0
@@ -130,8 +203,23 @@ def w():
     
     print(f"\n白质高信号体积最大病灶主体位于{brain_lobe[max3[0][0]]}，该叶内的白质高信号体积为：{round(max3[0][1]/1000,2)}立方厘米")
     print(f"其最大三维长径为{max_flair_3d[0]}厘米，对应的宽度为{max_flair_3d[1]}厘米，高度为{max_flair_3d[2]}厘米")
-    print(f"其轴位图像最大长径位于第{max_flair_2d[0]}层面，长度为{max_flair_2d[1]}厘米，宽度为{max_flair_2d[2]}厘米，面积为{max_flair_2d[3]}平方厘米")
-    print(f"其轴位图像最大面积位于第{max_flair_area[0]}层面，长度为{max_flair_area[1]}厘米，宽度为{max_flair_area[2]}厘米，面积为{max_flair_area[3]}平方厘米")
+    print(f"其真实三维长径为{round(true_max_flair_3d[0]/10,2)}厘米，两个端点的坐标ZYX为{true_max_flair_3d[1]}和{true_max_flair_3d[2]}")
+    #在t1图像中标记相应的坐标
+    t1[true_max_flair_3d[1][0],true_max_flair_3d[1][1],true_max_flair_3d[1][2]]=t1.max()+1000
+    t1[true_max_flair_3d[2][0],true_max_flair_3d[2][1],true_max_flair_3d[2][2]]=t1.max()+1000
+    
+    print(f"\n其轴位图像最大长径位于第{max_flair_2d[0]}层面，长度为{max_flair_2d[1]}厘米，宽度为{max_flair_2d[2]}厘米，面积为{max_flair_2d[3]}平方厘米")
+    print(f"其轴位图像真实长径位于第{true_max_flair_2d[0]}层面，长度为{round(true_max_flair_2d[1]/10,2)}厘米，两个端点的坐标ZYX为{true_max_flair_2d[2]}和{true_max_flair_2d[3]}")
+    #在t1图像中标记相应的坐标
+    t1[true_max_flair_2d[2][0],true_max_flair_2d[2][1],true_max_flair_2d[2][2]]=t1.max()+1000
+    t1[true_max_flair_2d[3][0],true_max_flair_2d[3][1],true_max_flair_2d[3][2]]=t1.max()+1000
+    
+    print(f"\n其轴位图像最大面积位于第{max_flair_area[0]}层面，长度为{max_flair_area[1]}厘米，宽度为{max_flair_area[2]}厘米，面积为{max_flair_area[3]}平方厘米")
+    print(f"其真实长径为{max_flair_area[4]}厘米，，两个端点的坐标ZYX为{max_flair_area[5]}和{max_flair_area[6]}")
+    #在t1图像中标记相应的坐标
+    t1[max_flair_area[5][0],max_flair_area[5][1],max_flair_area[5][2]]=t1.max()+1000
+    t1[max_flair_area[6][0],max_flair_area[6][1],max_flair_area[6][2]]=t1.max()+1000
+    slicer.util.updateVolumeFromArray(t1_node,t1)
     
     print()
     for i in sorted(c1):
