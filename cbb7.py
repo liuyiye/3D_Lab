2800e2e4986ea40d2a2038a2cd4c91c81586b503
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-01-19 06:23:03 -0800

    Add files via upload

--- cbb7.py
+++ cbb7.py
@@ -1,3 +1,44 @@
+def packages():
+  import profile
+  import smtplib
+  import sqlite3
+  import time
+  import timeit
+  import trace
+  import argparse
+  import asyncio
+  import codecs
+  import collections
+  import configparser
+  import copy
+  import csv
+  import datetime
+  import difflib
+  import getpass
+  import gettext
+  import io
+  import itertools
+  import json
+  import locale
+  import logging
+  import math
+  import mmap
+  import multiprocessing
+  import os
+  import pickle
+  import pydoc
+  import random
+  import re
+  import shutil
+  import signal
+  import socket
+  import sys
+  import tempfile
+  import threading
+  import unittest
+  import webbrowser
+  import zlib
+
 def t3237(dcmdir='c:/0'):
   import os
   import pydicom
@@ -89,7 +130,7 @@ def c3d(dcmdir='c:/0'):
   import os
   import pydicom
   from pynetdicom import AE
-  from pynetdicom.sop_class import CTImageStorage,MRImageStorage,SecondaryCaptureImageStorage
+  from pynetdicom.sop_class import CTImageStorage,MRImageStorage,SecondaryCaptureImageStorage,XRayRadiofluoroscopicImageStorage,ComputedRadiographyImageStorage,DigitalMammographyXRayImageStorageForPresentation,DigitalXRayImageStorageForPresentation
   ae = AE(ae_title=b'C3D')
   ae.add_requested_context(CTImageStorage)
   ae.add_requested_context(CTImageStorage,[pydicom.uid.JPEGLosslessSV1])
@@ -97,6 +138,10 @@ def c3d(dcmdir='c:/0'):
   ae.add_requested_context(MRImageStorage,[pydicom.uid.JPEGLosslessSV1])
   ae.add_requested_context(SecondaryCaptureImageStorage)
   ae.add_requested_context(SecondaryCaptureImageStorage,[pydicom.uid.JPEGLosslessSV1])
+  ae.add_requested_context(XRayRadiofluoroscopicImageStorage)
+  ae.add_requested_context(ComputedRadiographyImageStorage)
+  ae.add_requested_context(DigitalMammographyXRayImageStorageForPresentation)
+  ae.add_requested_context(DigitalXRayImageStorageForPresentation)
   files=os.listdir(dcmdir)
   ds_all = [pydicom.dcmread(os.path.join(dcmdir, file),force=True) for file in files]
   assoc = ae.associate('192.168.21.16',2002,ae_title=b'SDM')
@@ -105,7 +150,34 @@ def c3d(dcmdir='c:/0'):
       status = assoc.send_c_store(ds)
     assoc.release()
 
-def exec_():
+def sft():
+  from win32file import CreateFile, GetFileTime, SetFileTime, CloseHandle
+  from win32file import GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING
+  from pywintypes import Time
+  from datetime import datetime
+  import win32timezone
+  
+  file=input(r'filename c:\0\1.txt: ') or 'c:\\0\\1.txt'
+  ct=input('createTime datetime(2023,9,17,9,17,9): ') or 'datetime(2023,9,17,9,17,9)'
+  at=input('accessTime datetime(2023,9,17,9,17,7): ') or 'datetime(2023,9,17,9,17,7)'
+  mt=input('modifyTime datetime(2023,9,17,9,17,1): ') or 'datetime(2023,9,17,9,17,1)'
+
+  c=Time(eval(ct).timestamp())
+  a=Time(eval(at).timestamp())
+  m=Time(eval(mt).timestamp())
+  
+  fh = CreateFile(file, GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
+  SetFileTime(fh, c,a,m)
+  CloseHandle(fh)
+
+def detect_encoding(filename):
+    import chardet
+    with open(filename, 'rb') as f: 
+        rawdata = f.read(10000)
+    result = chardet.detect(rawdata)
+    return result['encoding']
+
+def exec_file():
   import io
   import os
   import sys
@@ -117,11 +189,12 @@ def exec_():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('filename: ')
-  with open(a,'r') as f:
+  import codecs
+  a=input(r'filename:')
+  with codecs.open(a, encoding=detect_encoding(a)) as f:
     exec(f.read())
 
-def exec8():
+def line():
   import io
   import os
   import sys
@@ -133,32 +206,15 @@ def exec8():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('filename: ')
-  with open(a,'r',encoding='utf-8') as f:
-    exec(f.read())
-
-def execlines():
-  import io
-  import os
-  import sys
-  import csv
-  import time
-  import msvcrt
-  import shutil
-  import PIL
-  import numpy
-  import pydicom
-  import pynetdicom
-  print('lines:')
-  mem_file=io.StringIO()
+  import codecs
   while True:
-    line=input()
-    if line=='':break
-    mem_file.write(line+'\n')
-  code=mem_file.getvalue()
-  exec(code)
+    try:
+      code=input('line>>>')
+      if code=='r':return
+      exec(code)
+    except Exception as e:print(e)
 
-def eval_():
+def lines():
   import io
   import os
   import sys
@@ -170,11 +226,63 @@ def eval_():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('line: \n')
-  print(eval(a))
-
+  import codecs
+  while True:
+    try:
+      print('lines:')
+      mem_file=io.StringIO()
+      while True:
+        i=input()
+        if i=='':break
+        if i=='r':return
+        mem_file.write(i+'\n')
+      code=mem_file.getvalue()
+      exec(code)
+    except Exception as e:print(e)
+    
 def h():
   print(r'''
+def packages():
+  import profile
+  import smtplib
+  import sqlite3
+  import time
+  import timeit
+  import trace
+  import argparse
+  import asyncio
+  import codecs
+  import collections
+  import configparser
+  import copy
+  import csv
+  import datetime
+  import difflib
+  import getpass
+  import gettext
+  import io
+  import itertools
+  import json
+  import locale
+  import logging
+  import math
+  import mmap
+  import multiprocessing
+  import os
+  import pickle
+  import pydoc
+  import random
+  import re
+  import shutil
+  import signal
+  import socket
+  import sys
+  import tempfile
+  import threading
+  import unittest
+  import webbrowser
+  import zlib
+
 def t3237(dcmdir='c:/0'):
   import os
   import pydicom
@@ -266,7 +374,7 @@ def c3d(dcmdir='c:/0'):
   import os
   import pydicom
   from pynetdicom import AE
-  from pynetdicom.sop_class import CTImageStorage,MRImageStorage,SecondaryCaptureImageStorage
+  from pynetdicom.sop_class import CTImageStorage,MRImageStorage,SecondaryCaptureImageStorage,XRayRadiofluoroscopicImageStorage,ComputedRadiographyImageStorage,DigitalMammographyXRayImageStorageForPresentation,DigitalXRayImageStorageForPresentation
   ae = AE(ae_title=b'C3D')
   ae.add_requested_context(CTImageStorage)
   ae.add_requested_context(CTImageStorage,[pydicom.uid.JPEGLosslessSV1])
@@ -274,6 +382,10 @@ def c3d(dcmdir='c:/0'):
   ae.add_requested_context(MRImageStorage,[pydicom.uid.JPEGLosslessSV1])
   ae.add_requested_context(SecondaryCaptureImageStorage)
   ae.add_requested_context(SecondaryCaptureImageStorage,[pydicom.uid.JPEGLosslessSV1])
+  ae.add_requested_context(XRayRadiofluoroscopicImageStorage)
+  ae.add_requested_context(ComputedRadiographyImageStorage)
+  ae.add_requested_context(DigitalMammographyXRayImageStorageForPresentation)
+  ae.add_requested_context(DigitalXRayImageStorageForPresentation)
   files=os.listdir(dcmdir)
   ds_all = [pydicom.dcmread(os.path.join(dcmdir, file),force=True) for file in files]
   assoc = ae.associate('192.168.21.16',2002,ae_title=b'SDM')
@@ -282,7 +394,34 @@ def c3d(dcmdir='c:/0'):
       status = assoc.send_c_store(ds)
     assoc.release()
 
-def exec_():
+def sft():
+  from win32file import CreateFile, GetFileTime, SetFileTime, CloseHandle
+  from win32file import GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING
+  from pywintypes import Time
+  from datetime import datetime
+  import win32timezone
+  
+  file=input(r'filename c:\0\1.txt: ') or 'c:\\0\\1.txt'
+  ct=input('createTime datetime(2023,9,17,9,17,9): ') or 'datetime(2023,9,17,9,17,9)'
+  at=input('accessTime datetime(2023,9,17,9,17,7): ') or 'datetime(2023,9,17,9,17,7)'
+  mt=input('modifyTime datetime(2023,9,17,9,17,1): ') or 'datetime(2023,9,17,9,17,1)'
+
+  c=Time(eval(ct).timestamp())
+  a=Time(eval(at).timestamp())
+  m=Time(eval(mt).timestamp())
+  
+  fh = CreateFile(file, GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
+  SetFileTime(fh, c,a,m)
+  CloseHandle(fh)
+
+def detect_encoding(filename):
+    import chardet
+    with open(filename, 'rb') as f: 
+        rawdata = f.read(10000)
+    result = chardet.detect(rawdata)
+    return result['encoding']
+
+def exec_file():
   import io
   import os
   import sys
@@ -294,11 +433,12 @@ def exec_():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('filename: ')
-  with open(a,'r') as f:
+  import codecs
+  a=input(r'filename:')
+  with codecs.open(a, encoding=detect_encoding(a)) as f:
     exec(f.read())
 
-def exec8():
+def line():
   import io
   import os
   import sys
@@ -310,32 +450,15 @@ def exec8():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('filename: ')
-  with open(a,'r',encoding='utf-8') as f:
-    exec(f.read())
-
-def execlines():
-  import io
-  import os
-  import sys
-  import csv
-  import time
-  import msvcrt
-  import shutil
-  import PIL
-  import numpy
-  import pydicom
-  import pynetdicom
-  print('lines:')
-  mem_file=io.StringIO()
+  import codecs
   while True:
-    line=input()
-    if line=='':break
-    mem_file.write(line+'\n')
-  code=mem_file.getvalue()
-  exec(code)
+    try:
+      code=input('line>>>')
+      if code=='r':return
+      exec(code)
+    except Exception as e:print(e)
 
-def eval_():
+def lines():
   import io
   import os
   import sys
@@ -347,133 +470,159 @@ def eval_():
   import numpy
   import pydicom
   import pynetdicom
-  a=input('line: \n')
-  print(eval(a))
+  import codecs
+  while True:
+    try:
+      print('lines:')
+      mem_file=io.StringIO()
+      while True:
+        i=input()
+        if i=='':break
+        if i=='r':return
+        mem_file.write(i+'\n')
+      code=mem_file.getvalue()
+      exec(code)
+    except Exception as e:print(e)
 
 def h():
   print(r' ' '
 
     ' ' ')
 
-import msvcrt
-while True:
-  password = ''
+def main():
+  import msvcrt
   while True:
-    ch = msvcrt.getwch()
-    if ch == '\r':break
-    password += ch
-  if password == '2133f':break
+    password = ''
+    while True:
+      ch = msvcrt.getwch()
+      if ch == '\r':break
+      password += ch
+    if password == '2133f':break
 
-while True:
-  print("\nfunctions:")
-  print("0 - t3237      a - exec ansi GBK") 
-  print("1 - t3237w     b - exec utf-8 no BOM")
-  print("2 - t8w        c - exec lines")
-  print("3 - c3d        d - eval line")
-  print("6 - t3237csv   9 - exit\n")
-  choice = input("select:")
+  while True:
+    print("\nc:\\0 functions:")
+    print("0 - t3237      a - set file time") 
+    print("1 - t3237w     b - exec file")
+    print("2 - t8w        c - exec lines r return")
+    print("3 - c3d        d - exec line r return")
+    print("6 - t3237csv   9 - exit\n")
+    choice = input("select:")
 
-  if choice == '9': 
-    break
+    if choice == '9': 
+      break
 
-  elif choice == '0':
-    t3237()
+    elif choice == '0':
+      t3237()
 
-  elif choice == '1':
-    t3237w()
+    elif choice == '1':
+      t3237w()
 
-  elif choice == '2':
-    t8w()
+    elif choice == '2':
+      t8w()
 
-  elif choice == '3':
-    c3d()
+    elif choice == '3':
+      c3d()
 
-  elif choice == '6':
-    t3237csv()
+    elif choice == '6':
+      t3237csv()
 
-  elif choice == 'a':
-    try:exec_()
-    except Exception as e:print(e)
+    elif choice == 'a':
+      try:sft()
+      except Exception as e:print(e)
 
-  elif choice == 'b':
-    try:exec8()
-    except Exception as e:print(e)
+    elif choice == 'b':
+      try:exec_file()
+      except Exception as e:print(e)
 
-  elif choice == 'c':
-    try:execlines()
-    except Exception as e:print(e)
+    elif choice == 'c':
+      try:lines()
+      except Exception as e:print(e)
 
-  elif choice == 'd':
-    try:eval_()
-    except Exception as e:print(e)
+    elif choice == 'd':
+      try:line()
+      except Exception as e:print(e)
 
-  elif choice=='h':
-    h()
+    elif choice=='h':
+      h()
 
-  else:
-    print(choice)
+    else:
+      print(choice)
 
-print("exit!")
+  print("exit!")
+
+import sys
+import codecs
+if len(sys.argv)>1:
+  with codecs.open(sys.argv[1], encoding=detect_encoding(sys.argv[1])) as f:
+    exec(f.read())
+else:main()
 
     ''')
 
-import msvcrt
-while True:
-  password = ''
+def main():
+  import msvcrt
   while True:
-    ch = msvcrt.getwch()
-    if ch == '\r':break
-    password += ch
-  if password == '2133f':break
+    password = ''
+    while True:
+      ch = msvcrt.getwch()
+      if ch == '\r':break
+      password += ch
+    if password == '2133f':break
 
-while True:
-  print("\nfunctions:")
-  print("0 - t3237      a - exec ansi GBK") 
-  print("1 - t3237w     b - exec utf-8 no BOM")
-  print("2 - t8w        c - exec lines")
-  print("3 - c3d        d - eval line")
-  print("6 - t3237csv   9 - exit\n")
-  choice = input("select:")
+  while True:
+    print("\nc:\\0 functions:")
+    print("0 - t3237      a - set file time") 
+    print("1 - t3237w     b - exec file")
+    print("2 - t8w        c - exec lines r return")
+    print("3 - c3d        d - exec line r return")
+    print("6 - t3237csv   9 - exit\n")
+    choice = input("select:")
 
-  if choice == '9': 
-    break
+    if choice == '9': 
+      break
 
-  elif choice == '0':
-    t3237()
+    elif choice == '0':
+      t3237()
 
-  elif choice == '1':
-    t3237w()
+    elif choice == '1':
+      t3237w()
 
-  elif choice == '2':
-    t8w()
+    elif choice == '2':
+      t8w()
 
-  elif choice == '3':
-    c3d()
+    elif choice == '3':
+      c3d()
 
-  elif choice == '6':
-    t3237csv()
+    elif choice == '6':
+      t3237csv()
 
-  elif choice == 'a':
-    try:exec_()
-    except Exception as e:print(e)
+    elif choice == 'a':
+      try:sft()
+      except Exception as e:print(e)
 
-  elif choice == 'b':
-    try:exec8()
-    except Exception as e:print(e)
+    elif choice == 'b':
+      try:exec_file()
+      except Exception as e:print(e)
 
-  elif choice == 'c':
-    try:execlines()
-    except Exception as e:print(e)
+    elif choice == 'c':
+      try:lines()
+      except Exception as e:print(e)
 
-  elif choice == 'd':
-    try:eval_()
-    except Exception as e:print(e)
+    elif choice == 'd':
+      try:line()
+      except Exception as e:print(e)
 
-  elif choice=='h':
-    h()
+    elif choice=='h':
+      h()
 
-  else:
-    print(choice)
+    else:
+      print(choice)
 
-print("exit!")
+  print("exit!")
 
+import sys
+import codecs
+if len(sys.argv)>1:
+  with codecs.open(sys.argv[1], encoding=detect_encoding(sys.argv[1])) as f:
+    exec(f.read())
+else:main()
