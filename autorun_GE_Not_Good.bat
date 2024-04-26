1bab38b8d9679c9c1487bdacada786abd8f79a8b
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-04-26 05:11:27 -0800

    Add files via upload

--- autorun_GE_Not_Good.bat
+++ autorun_GE_Not_Good.bat
@@ -1,12 +1,10 @@
-REM 获取当前日期并将其格式化为 YYYY-MM-DD-TIME
 for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /format:list') do set datetime=%%a
 set datestamp=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%-%datetime:~8,6%
 
 rename C:\GE_Not_Good\GE_Not_Good.log %datestamp%.log
-"%ProgramFiles%\WinRAR\RAR.exe" a -ep1 -o- C:\GE_Not_Good\log.zip C:\GE_Not_Good\%datestamp%.log
-del C:\GE_Not_Good\%datestamp%.log
+move C:\GE_Not_Good\%datestamp%.log C:\GE_Not_Good\log\
 
-robocopy  C:\GE_Not_Good\storage  C:\GE_Not_Good\not_finished /E /MOVE /IS
+robocopy  C:\GE_Not_Good\storage  C:\GE_Not_Good\not_finished /E /MOVE /IS > nul
 mkdir C:\GE_Not_Good\storage
 
 python C:\GE_Not_Good\GE_Not_Good.py
