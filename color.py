d307a558cb5034865137fdf8de28de508b2bfc55
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-06-02 04:03:12 -0700

    Add files via upload

--- color.py
+++ color.py
@@ -1,57 +1,60 @@
-
-import os
-import pydicom
-import numpy as np
+from pynetdicom import AE, evt, StoragePresentationContexts
 import matplotlib.pyplot as plt
-
-dicom_folder = r'C:\0'
-dst_folder = r'C:\1'
-if not os.path.exists(dst_folder):
-    os.makedirs(dst_folder)
+import pydicom,numpy as np
 
 cmap = plt.get_cmap('jet')
 
-for root, dirs, files in os.walk(dicom_folder):
-    for file in files:
-        src_file = os.path.join(root, file)
-        try:    
-            ds=pydicom.dcmread(src_file)
-            is_dicom = True
-        except:
-            is_dicom = False
-        if is_dicom:
-            pixel_data = ds.pixel_array
-            if 'RedPaletteColorLookupTableData' in ds:
-                width = ds.Rows
-                height = ds.Columns
-                red_clut = ds.RedPaletteColorLookupTableData
-                green_clut = ds.GreenPaletteColorLookupTableData
-                blue_clut = ds.BluePaletteColorLookupTableData
-                rgb_data = np.zeros((height, width, 3), dtype=np.uint8)
-                for i in range(height):
-                    for j in range(width):
-                        index = pixel_data[i, j]
-                        rgb_data[i, j, 0] = red_clut[index]
-                        rgb_data[i, j, 1] = green_clut[index]
-                        rgb_data[i, j, 2] = blue_clut[index]
-            else:
-                normalized_pixel_data = (pixel_data - np.min(pixel_data)) / (np.max(pixel_data) - np.min(pixel_data))
-                rgb_data = cmap(normalized_pixel_data)[:, :, :3]
-                rgb_data = (rgb_data * 255).astype(np.uint8)
-            ds.PhotometricInterpretation = 'RGB'
-            ds.SamplesPerPixel = 3
-            ds.BitsAllocated = 8
-            ds.BitsStored = 8
-            ds.HighBit = 7
-            ds.PixelRepresentation = 0
-            ds.PixelData = rgb_data.tobytes()
-            ds.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
-            relative_path = os.path.relpath(src_file, dicom_folder)
-            dst_path = os.path.join(dst_folder, relative_path)
-            dst_dir = os.path.dirname(dst_path)
-            if not os.path.exists(dst_dir):
-                  os.makedirs(dst_dir)
-            ds.save_as(dst_path)
+def handle_store(event):
+    ds = event.dataset
+    ds.file_meta = event.file_meta
+    pixel_data = ds.pixel_array
+    if 'RedPaletteColorLookupTableData' in ds:
+        width = ds.Rows
+        height = ds.Columns
+        red_clut = ds.RedPaletteColorLookupTableData
+        green_clut = ds.GreenPaletteColorLookupTableData
+        blue_clut = ds.BluePaletteColorLookupTableData
+        rgb_data = np.zeros((height, width, 3), dtype=np.uint8)
+        for i in range(height):
+            for j in range(width):
+                index = pixel_data[i, j]
+                rgb_data[i, j, 0] = red_clut[index]
+                rgb_data[i, j, 1] = green_clut[index]
+                rgb_data[i, j, 2] = blue_clut[index]
+    else:
+        normalized_pixel_data = (pixel_data - np.min(pixel_data)) / (np.max(pixel_data) - np.min(pixel_data))
+        rgb_data = cmap(normalized_pixel_data)[:, :, :3]
+        rgb_data = (rgb_data * 255).astype(np.uint8)
+    
+    ds.PhotometricInterpretation = 'RGB'
+    ds.SamplesPerPixel = 3
+    ds.BitsAllocated = 8
+    ds.BitsStored = 8
+    ds.HighBit = 7
+    ds.PixelRepresentation = 0
+    ds.PixelData = rgb_data.tobytes()
+    ds.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
+    
+    ds[0x00080018].value = pydicom.uid.generate_uid()
+    ds[0x0008103e].value = '3D_Lab_'+ds[0x0008103e].value
+    
+    uid=ds[0x0020000e].value
+    root = uid[:-1]
+    last_digit = int(uid[-1])
+    new_last_digit = (last_digit + 2) % 10
+    modified_uid = root + str(new_last_digit)
+    ds[0x0020000e].value = modified_uid
+    
+    assoc = ae.associate('172.19.83.228',4006,ae_title=b'AE_TITLE')
+    if assoc.is_established:
+        status = assoc.send_c_store(ds)
+        assoc.release()
+    return 0x0000
 
+handlers = [(evt.EVT_C_STORE, handle_store)]
+ae = AE(ae_title=b'Color')
+for context in StoragePresentationContexts:
+    ae.add_supported_context(context.abstract_syntax)
+    ae.add_requested_context(context.abstract_syntax)
 
-                
+ae.start_server(('', 11112), block=False, evt_handlers=handlers)
