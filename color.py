917877ee1e999bfb8e7a2e131cd71c6bb4afa26f
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-06-02 00:14:49 -0700

    Add files via upload

--- color.py
+++ color.py
@@ -2,12 +2,14 @@
 import os
 import pydicom
 import numpy as np
-import colorsys
+import matplotlib.pyplot as plt
 
 dicom_folder = r'C:\0'
 dst_folder = r'C:\1'
 if not os.path.exists(dst_folder):
     os.makedirs(dst_folder)
+
+cmap = plt.get_cmap('jet')
 
 for root, dirs, files in os.walk(dicom_folder):
     for file in files:
@@ -19,23 +21,37 @@ for root, dirs, files in os.walk(dicom_folder):
             is_dicom = False
         if is_dicom:
             pixel_data = ds.pixel_array
-            normalized_pixel_data = (pixel_data - np.min(pixel_data)) / (np.max(pixel_data) - np.min(pixel_data))
-            colored_pixel_data = np.zeros((ds.Rows, ds.Columns, 3), dtype=np.float64)
-            for row in range(ds.Rows):
-                for col in range(ds.Columns):
-                    hue = normalized_pixel_data[row, col]
-                    colored_pixel_data[row, col, :] = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-            colored_pixel_data = (colored_pixel_data * 255).astype(np.uint8) # 将颜色数据转换为8位整数格式
-            ds.PixelData = colored_pixel_data.tobytes()
-            ds.SamplesPerPixel = 3
+            if 'RedPaletteColorLookupTableData' in ds:
+                width = ds.Rows
+                height = ds.Columns
+                red_clut = ds.RedPaletteColorLookupTableData
+                green_clut = ds.GreenPaletteColorLookupTableData
+                blue_clut = ds.BluePaletteColorLookupTableData
+                rgb_data = np.zeros((height, width, 3), dtype=np.uint8)
+                for i in range(height):
+                    for j in range(width):
+                        index = pixel_data[i, j]
+                        rgb_data[i, j, 0] = red_clut[index]
+                        rgb_data[i, j, 1] = green_clut[index]
+                        rgb_data[i, j, 2] = blue_clut[index]
+            else:
+                normalized_pixel_data = (pixel_data - np.min(pixel_data)) / (np.max(pixel_data) - np.min(pixel_data))
+                rgb_data = cmap(normalized_pixel_data)[:, :, :3]
+                rgb_data = (rgb_data * 255).astype(np.uint8)
             ds.PhotometricInterpretation = 'RGB'
+            ds.SamplesPerPixel = 3
             ds.BitsAllocated = 8
             ds.BitsStored = 8
             ds.HighBit = 7
-            ds.PixelRepresentation = 0  # 无符号整数
+            ds.PixelRepresentation = 0
+            ds.PixelData = rgb_data.tobytes()
+            ds.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
             relative_path = os.path.relpath(src_file, dicom_folder)
             dst_path = os.path.join(dst_folder, relative_path)
             dst_dir = os.path.dirname(dst_path)
             if not os.path.exists(dst_dir):
                   os.makedirs(dst_dir)
             ds.save_as(dst_path)
+
+
+                
