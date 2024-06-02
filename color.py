a961f35292411a6b317cec870db8c66f8a39cd18
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-06-02 08:50:02 -0700

    Add files via upload

--- color.py
+++ color.py
@@ -1,14 +1,18 @@
 from pynetdicom import AE, evt, StoragePresentationContexts
 import matplotlib.pyplot as plt
-import pydicom,numpy as np
+import pydicom,logging,numpy as np
+
+logging.basicConfig(
+    filename=r'C:\Color\color.log',
+    level=logging.WARNING,
+    format='%(asctime)s - %(levelname)s - %(message)s')
 
 cmap = plt.get_cmap('jet')
 
-def handle_store(event):
-    ds = event.dataset
-    ds.file_meta = event.file_meta
+def rgb(ds):
     pixel_data = ds.pixel_array
     if 'RedPaletteColorLookupTableData' in ds:
+        logging.warning(f'Palette {ds.PatientID,ds.StudyDate,ds.SeriesNumber,ds.SeriesDescription}')
         width = ds.Rows
         height = ds.Columns
         red_clut = ds.RedPaletteColorLookupTableData
@@ -22,6 +26,7 @@ def handle_store(event):
                 rgb_data[i, j, 1] = green_clut[index]
                 rgb_data[i, j, 2] = blue_clut[index]
     else:
+        logging.warning(f'cmap {ds.PatientID,ds.StudyDate,ds.SeriesNumber,ds.SeriesDescription}')
         normalized_pixel_data = (pixel_data - np.min(pixel_data)) / (np.max(pixel_data) - np.min(pixel_data))
         rgb_data = cmap(normalized_pixel_data)[:, :, :3]
         rgb_data = (rgb_data * 255).astype(np.uint8)
@@ -51,8 +56,18 @@ def handle_store(event):
         assoc.release()
     return 0x0000
 
+def handle_store(event):
+    ds = event.dataset
+    ds.file_meta = event.file_meta
+    if ds.PhotometricInterpretation == 'RGB':
+        logging.warning(f'discard {ds.PatientID,ds.StudyDate,ds.SeriesNumber,ds.SeriesDescription}')
+    else:
+        rgb(ds)
+    return 0x0000
+
 handlers = [(evt.EVT_C_STORE, handle_store)]
 ae = AE(ae_title=b'Color')
+
 for context in StoragePresentationContexts:
     ae.add_supported_context(context.abstract_syntax)
     ae.add_requested_context(context.abstract_syntax)
