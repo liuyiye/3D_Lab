29c720370b82b412ec80b708bc96f5ec5fb17fdb
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-12-08 10:33:18 +0800

    Add files via upload

---
+++ scp.py
@@ -0,0 +1,19 @@
+import os,pydicom
+from pynetdicom import AE, evt, AllStoragePresentationContexts
+
+store_folder=r'D:\SCP'
+
+def handle_store(event):
+    ds = event.dataset
+    ds.file_meta = event.file_meta
+    pdir = os.path.join(store_folder, ds.PatientID)
+    f = os.path.join(pdir, f'{ds.SOPInstanceUID}.dcm')
+    os.makedirs(pdir, exist_ok=True)
+    ds.save_as(f, write_like_original=False)
+    return 0x0000
+
+handlers = [(evt.EVT_C_STORE, handle_store)]
+
+ae = AE(ae_title=b'C3D')
+ae.supported_contexts = AllStoragePresentationContexts
+ae.start_server(("", 11112), block=True, evt_handlers=handlers)
