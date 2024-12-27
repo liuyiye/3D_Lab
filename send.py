6e7896918d304de9dfb8f6cfc3603f0c95823756
liuyiye <133562379+liuyiye@users.noreply.github.com>
2024-12-27 16:39:39 +0800

    Add files via upload

--- send.py
+++ send.py
@@ -1,31 +1,67 @@
-﻿
-def send(IP,PORT,AET,PATH):
+﻿def send(IP,PORT,AET,PATH):
   import os
   import pydicom
-  from pynetdicom import AE
-  from pynetdicom.sop_class import CTImageStorage,MRImageStorage,SecondaryCaptureImageStorage
+  from pynetdicom import AE,debug_logger
+  #debug_logger()
+  from pynetdicom.sop_class import (
+    RawDataStorage,          # 1.2.840.10008.5.1.4.1.1.66
+    CTImageStorage,          # 1.2.840.10008.5.1.4.1.1.2
+    MRImageStorage,          # 1.2.840.10008.5.1.4.1.1.4
+    SecondaryCaptureImageStorage,  # 1.2.840.10008.5.1.4.1.1.7
+    XRayAngiographicImageStorage,  # 1.2.840.10008.5.1.4.1.1.12.2
+    DigitalMammographyXRayImageStorageForPresentation,  # 1.2.840.10008.5.1.4.1.1.1.2
+    DigitalXRayImageStorageForPresentation,  # 1.2.840.10008.5.1.4.1.1.1.1
+    NuclearMedicineImageStorage,            # 1.2.840.10008.5.1.4.1.1.20
+    PositronEmissionTomographyImageStorage, # 1.2.840.10008.5.1.4.1.1.128
+  )
+  from pydicom.uid import (
+    ExplicitVRLittleEndian, #1.2.840.10008.1.2.1
+    ImplicitVRLittleEndian, #1.2.840.10008.1.2
+    #JPEGLossless,   #1.2.840.10008.1.2.4.70
+    JPEGLosslessSV1 #1.2.840.10008.1.2.4.57
+  )
   
   ae = AE(ae_title=b'SDM')
-  ae.add_requested_context(CTImageStorage)
-  ae.add_requested_context(CTImageStorage,[pydicom.uid.JPEGLosslessSV1])
-  ae.add_requested_context(MRImageStorage)
-  ae.add_requested_context(MRImageStorage,[pydicom.uid.JPEGLosslessSV1])
-  ae.add_requested_context(SecondaryCaptureImageStorage)
-  ae.add_requested_context(SecondaryCaptureImageStorage,[pydicom.uid.JPEGLosslessSV1])
+  
+  transfer_syntaxes = [
+    #JPEGLossless,
+    JPEGLosslessSV1,
+    ExplicitVRLittleEndian,
+    ImplicitVRLittleEndian
+  ]
+  
+  storage_classes = [
+    RawDataStorage,
+    CTImageStorage,
+    MRImageStorage,
+    XRayAngiographicImageStorage,
+    DigitalMammographyXRayImageStorageForPresentation,
+    DigitalXRayImageStorageForPresentation,
+    SecondaryCaptureImageStorage,
+    NuclearMedicineImageStorage,
+    PositronEmissionTomographyImageStorage,
+  ]
+  
+  for storage_class in storage_classes:
+    ae.add_requested_context(storage_class, transfer_syntaxes)
   
   assoc = ae.associate(IP,PORT,ae_title=AET)
   if assoc.is_established:
     for root, dirs, files in os.walk(PATH):
       n=0
       for f in files:
+        if f.upper() in ['DIRFILE', 'DICOMDIR']:
+            continue
         try:
           ds = pydicom.dcmread(os.path.join(root, f))
           is_dicom = True
         except:
           is_dicom = False
         if is_dicom:
-          status = assoc.send_c_store(ds)
-          n = n+1
+          try:
+            status = assoc.send_c_store(ds)
+            n = n+1
+          except:pass
       print(f'{n} dicom files sent. {root}')
     assoc.release()
 
@@ -84,8 +120,12 @@ while True:
     try:send('172.20.99.82', 11112, 'C3D',PATH)
     except Exception as e:print(e)
   
+  elif choice == '3d':
+    try:send('172.20.99.33', 11112, '3D',PATH)
+    except Exception as e:print(e)
+  
   elif choice == 'i':
-    print("1p.2c.3u,82") 
+    print("1p.2c.3u,82,3d") 
     IP = input("please input IP: ")
     try:PORT = int(input("please input PORT: "))
     except:pass
