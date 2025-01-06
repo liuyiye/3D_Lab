439ad41f1428e4544ab471ef21c598e0a163a4e0
liuyiye <133562379+liuyiye@users.noreply.github.com>
2025-01-06 11:01:53 +0800

    Add files via upload

--- color_local.py
+++ color_local.py
@@ -1,76 +1,123 @@
+import msvcrt
+while True:
+  password = ''
+  while True:
+    ch = msvcrt.getwch()
+    if ch == '\r':break
+    password += ch
+  if password == '678':break
+
+while True:
+    convert = input("Convert images in the current folder to color images. return for not.") or False
+    if convert:break
+
 import os,pydicom,numpy as np
 from skimage.transform import resize
 
-dicom_folder = '/home/edu/0'
-dst_folder = '/home/edu/1'
-if not os.path.exists(dst_folder):
-    os.makedirs(dst_folder)
+dicom_folder = '.'
+dst_folder = 'color_output'
 
-for root, dirs, files in os.walk(dicom_folder):
-    for f in files:
-        src_file = os.path.join(root, f)
-        try:    
-            ds=pydicom.dcmread(src_file)
-            series_dir = os.path.join(dst_folder, ds.SeriesInstanceUID)
-            os.makedirs(series_dir, exist_ok=True)
-            file_path = os.path.join(series_dir, f'{ds.SOPInstanceUID}.dcm')
-            ds.save_as(file_path, write_like_original=False)
-        except:
-            pass # or continue
-
-palette = np.genfromtxt('/home/edu/Color/palette1275.csv', delimiter=',', skip_header=0)
+palette = np.genfromtxt('palette1275.csv', delimiter=',', skip_header=0)
 def p(x):
     return(palette[x])
 
-for root, dirs, files in os.walk(dst_folder):
-    for d in dirs:
-        files = [os.path.join(root,d,f) for f in os.listdir(os.path.join(root,d))]
-        datas = np.array([pydicom.dcmread(f).pixel_array for f in files])
-        datas1,datas97,datas98,datas99 = np.percentile(datas,[1,97,98,99])
-        for f in files:
-            ds=pydicom.dcmread(f)
-            if ds.PhotometricInterpretation != 'RGB':
-                pixel_data = ds.pixel_array
-                
-                if min(ds.Rows,ds.Columns) < 256:
-                    normalized_data = pixel_data / np.max(pixel_data)
-                    resized_data = resize(normalized_data, (2*ds.Rows, 2*ds.Columns), mode='reflect', anti_aliasing=True)
-                    pixel_data = (resized_data * np.max(pixel_data)).astype(np.uint16)
-                    ds.Rows, ds.Columns = pixel_data.shape
-                
-                if ds.Modality == 'CT':
-                    try:
-                        WindowCenter,WindowWidth = ds.WindowCenter[0],ds.WindowWidth[0]
-                    except:
-                        WindowCenter,WindowWidth = ds.WindowCenter,ds.WindowWidth
-                    d = ds.RescaleIntercept
-                    pixel_data = pixel_data + d
-                    lower = max(WindowCenter - WindowWidth / 2, datas1 + d)
-                    upper = min(WindowCenter + WindowWidth / 2, datas99 + d)
-                elif 'ttp' in ds.SeriesDescription.lower():
-                    lower = datas1
-                    upper = datas97
-                elif 'mtt' in ds.SeriesDescription.lower():
-                    lower = datas1
-                    upper = datas98
-                else:
-                    lower = datas1
-                    upper = datas99
-                
-                if lower==upper:
-                    upper += 1
-                normalized_data = np.clip((pixel_data - lower) / (upper - lower),0,1)
-                grey_data = (normalized_data * 1274).astype(np.uint16)
-                rgb_data = p(grey_data).astype(np.uint8)
-                
-                if 'RescaleIntercept' in ds:
-                    ds.RescaleIntercept = 0
-                ds.PhotometricInterpretation = 'RGB'
-                ds.SamplesPerPixel = 3
-                ds.BitsAllocated = 8
-                ds.BitsStored = 8
-                ds.HighBit = 7
-                ds.PixelRepresentation = 0
-                ds.PixelData = rgb_data.tobytes()
-                ds.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
-                ds.save_as(f)
+def color():
+  for root, dirs, files in os.walk(dicom_folder):
+      siuid = pydicom.uid.generate_uid()
+      for file in files:
+          src_file = os.path.join(root, file)
+          try:    
+              ds = pydicom.dcmread(src_file)
+              is_dicom = True
+          except:
+              is_dicom = False
+          if is_dicom and ds.PhotometricInterpretation != 'RGB':
+              pixel_data = ds.pixel_array
+              datas1,datas97,datas98,datas99 = np.percentile(pixel_data,[1,97,98,99])
+              
+              if min(ds.Rows,ds.Columns) < 256:
+                  normalized_data = pixel_data / np.max(pixel_data)
+                  resized_data = resize(normalized_data, (2*ds.Rows, 2*ds.Columns), mode='reflect', anti_aliasing=True)
+                  pixel_data = (resized_data * np.max(pixel_data)).astype(np.uint16)
+                  ds.Rows, ds.Columns = pixel_data.shape
+              
+              if ds.Modality == 'CT':
+                  try:
+                      WindowCenter,WindowWidth = ds.WindowCenter[0],ds.WindowWidth[0]
+                  except:
+                      WindowCenter,WindowWidth = ds.WindowCenter,ds.WindowWidth
+                  d = ds.RescaleIntercept
+                  pixel_data = pixel_data + d
+                  lower = max(WindowCenter - WindowWidth / 2, datas1 + d)
+                  upper = min(WindowCenter + WindowWidth / 2, datas99 + d)
+              elif 'ttp' in ds.SeriesDescription.lower():
+                  lower = datas1
+                  upper = datas97
+              elif 'mtt' in ds.SeriesDescription.lower():
+                  lower = datas1
+                  upper = datas98
+              else:
+                  lower = datas1
+                  upper = datas99
+              
+              if lower==upper:
+                  upper += 1
+              normalized_data = np.clip((pixel_data - lower) / (upper - lower),0,1)
+              grey_data = (normalized_data * 1274).astype(np.uint16)
+              rgb_data = p(grey_data).astype(np.uint8)
+              
+              data = pydicom.Dataset()
+              data.file_meta = pydicom.Dataset()
+              data.file_meta.TransferSyntaxUID = pydicom.uid.ExplicitVRLittleEndian
+              
+              data.SOPClassUID = ds.SOPClassUID
+              data.PatientID = ds.PatientID
+              data.StudyInstanceUID = ds.StudyInstanceUID
+              data.PatientName = ds.PatientName
+              data.StudyDate = ds.StudyDate
+              data.StudyTime = ds.StudyTime
+              data.ContentDate = ds.ContentDate
+              data.ContentTime = ds.ContentTime
+              data.AccessionNumber = ds.AccessionNumber
+              data.Modality = ds.Modality
+              data.SeriesNumber = ds.SeriesNumber
+              data.PatientBirthDate = ds.PatientBirthDate
+              data.ReferringPhysicianName = ds.ReferringPhysicianName
+              data.PatientSex = ds.PatientSex
+              
+              data.SeriesDescription='3D_Lab_'+ds.SeriesDescription
+              data.SeriesInstanceUID = siuid
+              data.SOPInstanceUID = pydicom.uid.generate_uid()
+              data.InstanceNumber = ds.InstanceNumber
+              data.Rows=ds.Rows
+              data.Columns=ds.Columns
+              data.PhotometricInterpretation = 'RGB'
+              data.PlanarConfiguration = 0
+              data.RescaleIntercept=0
+              data.SamplesPerPixel=3
+              data.BitsAllocated = 8
+              data.BitsStored = 8
+              data.HighBit = 7
+              data.PixelRepresentation = 0
+              data.PixelData = rgb_data.tobytes()
+              
+              data.SliceLocation=''
+              data.ImagePositionPatient = ''
+              data.ImageOrientationPatient = ''
+              data.NumberOfFrames = ''
+              data.PixelSpacing = ''
+              data.WindowCenter = ''
+              data.WindowWidth = ''
+              data.RescaleSlope = ''
+              data.RescaleType = ''
+              data.SpecificCharacterSet = 'GB18030'
+              
+              relative_path = os.path.relpath(src_file, dicom_folder)
+              dst_path = os.path.join(dst_folder, relative_path)
+              dst_dir = os.path.dirname(dst_path)
+              os.makedirs(dst_dir, exist_ok=True)
+              data.save_as(dst_path, write_like_original=False)
+
+if __name__ == '__main__':
+  color()
+  print('\nDone!')
